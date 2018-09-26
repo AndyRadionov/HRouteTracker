004d61bf3f62cee9b539df29cbfaf9bd85e734b3
Andrey Radionov <andyomsk@gmail.com>
2018-09-26 18:03:28 +0300

    Added dependecies

--- app/src/main/java/io/github/andyradionov/hroutetracker/MainActivity.kt
+++ app/src/main/java/io/github/andyradionov/hroutetracker/MainActivity.kt
@@ -2,11 +2,22 @@ package io.github.andyradionov.hroutetracker
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.location.LocationListener
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var connectionCallbacks: GoogleApiClient.ConnectionCallbacks
+    private lateinit var onConnectionFailedListener: GoogleApiClient.OnConnectionFailedListener
+    private lateinit var locationListener: LocationListener
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val list = mutableListOf(1,2)
+        list.forEach {
+            list.remove(0)
+        }
     }
 }
